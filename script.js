dcc788294ebf2520bc5742932fed14fc2b3f0635
mahamudsourov <sourovm554@gmail.com>
2025-05-28 09:50:33 +0600

    js_init

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function display(){
+    let a = 5;
+    let b = 3.5;
+    let c = "Hello";
+    let d = 'Hi';
+    let e = true;
+    const f = 5;
+    var g = 6;
+
     console.log('Button Clicked from external');
+
+    console.log(a);
+    console.log(a+b);
+    
+    console.log(a+ ' ' +b+ ' '+d);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
 }
